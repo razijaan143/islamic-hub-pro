77f5624c66c7a7af74904fca24a0b2cfdb4365af
razijaan143 <razijaan1431@gmail.com>
2026-06-18 16:38:05 +0500

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE='islamichub-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/islamic-hub-pro/index.html','/islamic-hub-pro/manifest.json']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
